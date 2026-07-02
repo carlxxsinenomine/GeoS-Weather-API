a10f3864fc003935d19ab3ddef8e52c2a25f8930
est_calexx <carlxxsinenomine@gmail.com>
2026-07-02 08:10:27 +0800

    feat: added a simple crawler func that navigates to the loc page

---
+++ w_scraper/AWSpider.py
@@ -0,0 +1,43 @@
+import json
+from urllib.request import Request
+
+import scrapy
+
+from typing import Any, AsyncIterator, Generator, AsyncGenerator
+
+from scrapy import Request
+from scrapy.http.response.text import TextResponse
+from scrapy.crawler import AsyncCrawlerProcess
+from urllib.parse import quote
+
+class AccuWeatherScraper(scrapy.Spider):
+    def __init__(self, location: str=None, **kwargs: Any):
+        super(AccuWeatherScraper).__init__(**kwargs)
+        self.location = location
+
+    name = 'accu_weather'
+    custom_settings = {
+        "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    }
+
+    async def start(self) -> AsyncGenerator[Request, None]:
+        curr_location = quote(self.location) # Para sa mga spaces; turned to its ascii equivalent or dunno code
+        api_url = f"https://www.accuweather.com/web-api/autocomplete?query={curr_location}&language=en-us"
+        yield scrapy.Request(url=api_url, callback=self.parse_suggestions)
+
+    def parse_suggestions(self, response: TextResponse):
+        search_results = json.loads(response.text)
+        if search_results:
+            first_match = search_results[0]
+            loc_key = first_match.get('key')
+            path = f"/web-api/three-day-redirect?key={loc_key}"
+            yield response.follow(url=path, callback=self.parse_homepage)
+
+    def parse_homepage(self):
+        pass
+
+if __name__ == "__main__":
+    _location = "Libon, PH"
+    process = AsyncCrawlerProcess()
+    process.crawl(AccuWeatherScraper, location=_location)
+    process.start()

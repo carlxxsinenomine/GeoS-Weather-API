9032a0e592b8d0d80337be976c5ab7b60d4f7a4c
est_calexx <carlxxsinenomine@gmail.com>
2026-07-02 10:12:45 +0800

    feat: successfully breached through accuweather's robots.txt(i hope)

--- w_scraper/AWSpider.py
+++ w_scraper/AWSpider.py
@@ -1,6 +1,4 @@
 import json
-from urllib.request import Request
-
 import scrapy
 
 from typing import Any, AsyncIterator, Generator, AsyncGenerator
@@ -10,18 +8,25 @@ from scrapy.http.response.text import TextResponse
 from scrapy.crawler import AsyncCrawlerProcess
 from urllib.parse import quote
 
+
 class AccuWeatherScraper(scrapy.Spider):
-    def __init__(self, location: str=None, **kwargs: Any):
-        super(AccuWeatherScraper).__init__(**kwargs)
+    def __init__(self, location: str = None, **kwargs: Any):
+        super().__init__(**kwargs)
         self.location = location
 
     name = 'accu_weather'
     custom_settings = {
-        "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "ROBOTSTXT_OBEY": False,
+        "AUTOTHROTTLE_ENABLED": True,
+        "AUTOTHROTTLE_START_DELAY": 5,
+        "AUTOTHROTTLE_MAX_DELAY": 60,
+        "AUTOTHROTTLE_TARGET_CONCURRENCY": 1.0,
+        "AUTOTHROTTLE_DEBUG": False
     }
 
     async def start(self) -> AsyncGenerator[Request, None]:
-        curr_location = quote(self.location) # Para sa mga spaces; turned to its ascii equivalent or dunno code
+        curr_location = quote(self.location)  # Para sa mga spaces; turned to its ascii equivalent or dunno code
         api_url = f"https://www.accuweather.com/web-api/autocomplete?query={curr_location}&language=en-us"
         yield scrapy.Request(url=api_url, callback=self.parse_suggestions)
 
@@ -33,8 +38,10 @@ class AccuWeatherScraper(scrapy.Spider):
             path = f"/web-api/three-day-redirect?key={loc_key}"
             yield response.follow(url=path, callback=self.parse_homepage)
 
-    def parse_homepage(self):
-        pass
+    def parse_homepage(self, response: TextResponse):
+        yield {"img": response.css("img.weather-icon").attrib['src'],
+               "temp": response.css("div.temp::text").get().strip()}
+
 
 if __name__ == "__main__":
     _location = "Libon, PH"
